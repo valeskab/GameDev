bacce32808670cb1f87495cf7f7c0037240528de
Valeska Bagaric <valeskabagaric@hotmail.com>
2022-08-30 20:50:35 -0600

    Added FloatData and TriggetEventBehavior Script

---
+++ ScriptableObjectsLab/Assets/Scenes/TriggerEventBehavior.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehavior : MonoBehaviour
+{
+   public UnityEvent triggerEnterEvent;
+
+   private void OnTriggerEnter(Collider other)
+   {
+      triggerEnterEvent.Invoke();
+   }
+}
