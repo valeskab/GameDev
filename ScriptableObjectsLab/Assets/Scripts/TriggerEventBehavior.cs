f713ec98e7b3efea5a0669789fb176cc74b4bfa4
Valeska Bagaric <valeskabagaric@hotmail.com>
2022-09-07 19:18:47 -0600

    Edit triggerEvent script

--- ScriptableObjectsLab/Assets/Scripts/TriggerEventBehavior.cs
+++ ScriptableObjectsLab/Assets/Scripts/TriggerEventBehavior.cs
@@ -1,11 +1,9 @@
-using System;
 using UnityEngine;
 using UnityEngine.Events;
-
 public class TriggerEventBehavior : MonoBehaviour
+
 {
    public UnityEvent triggerEnterEvent;
-
    private void OnTriggerEnter(Collider other)
    {
       triggerEnterEvent.Invoke();
