75cb1226efee755be7edbea769903d9e10fc6507
Valeska Bagaric <valeskabagaric@hotmail.com>
2022-10-16 00:24:17 -0600

    Got Dress Changer to work

--- MyProjectVB2DM/Assets/Scripts/CharacterManager.cs
+++ MyProjectVB2DM/Assets/Scripts/CharacterManager.cs
@@ -1,19 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CharacterManager : MonoBehaviour
 {
-    public ClosetDatabase dressobj;
+    public CharacterCreatorSO dressobj;
 
     public SpriteRenderer artworkSprite;
 
     private int selectedOption = 0;
     void Start()
     {
-        
+        UpdateDress(selectedOption);
     }
-    
-    public void 
+
+    public void NextOption()
+    {
+        selectedOption++;
+        if (selectedOption >= dressobj.DressCount)
+        {
+            selectedOption = 0;
+        }
+        
+        UpdateDress(selectedOption);
+    }
+
+    public void BackOption()
+    {
+        selectedOption--;
+        if (selectedOption < 0)
+        {
+            selectedOption = dressobj.DressCount -1;
+        }
+        
+        UpdateDress(selectedOption);
+    }
+
+    private void UpdateDress(int selectedOption)
+    {
+        ClosetDatabase dress = dressobj.GetDress(selectedOption);
+        artworkSprite.sprite = dress.characterDress;
+    }
 }
