35006a236ff77d426e7f7d6c96df988ec9f32786
Valeska Bagaric <valeskabagaric@hotmail.com>
2022-10-21 19:17:57 -0600

    Added Icons, Tabs, and Getting Rid of demo scripts

--- MyProjectVB2DM/Assets/Scripts/AccesoryID.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-[CreateAssetMenu]
-public class AccesoryID : ScriptableObject
-{
-    public Sprite artwork;
-}
--- MyProjectVB2DM/Assets/Scripts/DressID.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-[CreateAssetMenu]
-public class DressID : ScriptableObject
-{
-    public Sprite[] dress;
-}
