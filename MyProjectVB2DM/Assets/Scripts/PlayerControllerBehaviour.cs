55681654387b6dadd6d1482c4c016d47d43657ef
Valeska Bagaric <valeskabagaric@hotmail.com>
2022-12-04 05:06:34 -0700

    jump controller

--- MyProjectVB2DM/Assets/Scripts/PlayerControllerBehaviour.cs
+++ MyProjectVB2DM/Assets/Scripts/PlayerControllerBehaviour.cs
@@ -5,11 +5,31 @@ using UnityEngine;
 
 public class PlayerControllerBehaviour : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb;
+    [Header("PlayerStats")]
+    public float jumpHeight = 8f;
+    private Rigidbody2D rb;
+    
+    [Header("GroundCheck")]
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private float moveVelocity;
+    
+    
+    public AudioClip marker;
+    private AudioSource source;
 
-    private void FixedUpdate()
+    private void Start()
     {
-        throw new NotImplementedException();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
     }
 }
