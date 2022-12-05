2604a4ee33620b9517c2a0703cf449b26af59040
Valeska Bagaric <valeskabagaric@hotmail.com>
2022-12-05 04:50:30 -0700

    Added sound to character

--- MyProjectVB2DM/Assets/Scripts/PlayerControllerBehaviour.cs
+++ MyProjectVB2DM/Assets/Scripts/PlayerControllerBehaviour.cs
@@ -23,13 +23,17 @@ public class PlayerControllerBehaviour : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        source = GetComponent<AudioSource>();
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround); 
+        if(Input.GetKeyDown(KeyCode.Space)&& isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+            source.PlayOneShot(marker, 1.0f);
         }
     }
+    
 }
