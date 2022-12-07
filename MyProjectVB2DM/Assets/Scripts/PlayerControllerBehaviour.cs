e18397a5db8099beec37a22fce8524b596ab8fb5
Valeska Bagaric <valeskabagaric@hotmail.com>
2022-12-07 01:02:49 -0700

    Skin Shop

--- MyProjectVB2DM/Assets/Scripts/PlayerControllerBehaviour.cs
+++ MyProjectVB2DM/Assets/Scripts/PlayerControllerBehaviour.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class PlayerControllerBehaviour : MonoBehaviour
 {
+
+    private CharacterController characterController;
+
     [Header("PlayerStats")]
     public float jumpHeight = 8f;
     private Rigidbody rb;
@@ -14,26 +17,39 @@ public class PlayerControllerBehaviour : MonoBehaviour
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask whatIsGround;
-    private float moveVelocity;
+    [SerializeField]private float moveVelocity;
+    [SerializeField]private float gravity = 10f;
     
     
     public AudioClip marker;
     private AudioSource source;
 
-    private void Start()
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
-        source = GetComponent<AudioSource>();
+        characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
+        Jump();
+        PlayerMovement();
         //isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround); 
-        if(Input.GetKeyDown(KeyCode.Space))
+    }
+
+    private void PlayerMovement()
+    {
+        Vector3 newPos = new Vector3(moveVelocity, jumpHeight, 0f);
+        characterController.Move(newPos * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        if(characterController.isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
-            source.PlayOneShot(marker, 1.0f);
+            jumpHeight = 0f;
         }
+
+        jumpHeight -= gravity * Time.deltaTime;
     }
     
 }
