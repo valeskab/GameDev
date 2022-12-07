5e669d7aa94b1a27da9945d7d8603b6fa1be8ceb
Valeska Bagaric <valeskabagaric@hotmail.com>
2022-12-07 06:19:00 -0700

    Save

--- MyProjectVB2DM/Assets/Scripts/UI_Main.cs
+++ MyProjectVB2DM/Assets/Scripts/UI_Main.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class UI_Main : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void SwitchMenuTo(GameObject uiMenu)
     {
-        
-    }
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        uiMenu.SetActive(true);
     }
 }
