8c3702259781b55cdd7a68a7e329d98841792f51
Valeska Bagaric <valeskabagaric@hotmail.com>
2022-12-12 08:48:31 -0700

    Trying to make pooler work

--- MyProjectVB2DM/Assets/Scripts/PoolingBehaviour.cs
+++ MyProjectVB2DM/Assets/Scripts/PoolingBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,50 @@ using UnityEngine.Events;
 
 public class PoolingBehaviour : MonoBehaviour
 {
+    [SerializeField] private string poolerName;
     [SerializeField] private GameObject[] objsToCreate;
     [SerializeField] private int objsQuanity;
+
+    private List<GameObject> createdInstances = new List<GameObject>();
+    private GameObject PoolerContainer;
+
+    private void Awake()
+    {
+        PoolerContainer = new GameObject($"Pooler - {poolerName}");
+        CreatePooler();
+    }
+
+    private void CreatePooler()
+    {
+        for (int i = 0; i < objsToCreate.Length; i++)
+        {
+            for (int j = 0; j < objsQuanity; j++)
+            {
+                createdInstances.Add(AddInstancer(objsToCreate[i]));
+            }
+        }
+    }
+
+    private GameObject AddInstancer(GameObject obj)
+    {
+        GameObject newObj = Instantiate(obj, PoolerContainer.transform);
+        newObj.name = obj.name;
+        newObj.SetActive(false);
+        return newObj;
+    }
+
+    public GameObject ObtainPoolerInstancer(string nombre)
+    {
+        for (int i = 0; i < createdInstances.Count; i++)
+        {
+            if (createdInstances[i].name == nombre)
+            {
+                if (createdInstances[i].activeSelf == false)
+                {
+                    return createdInstances[i];
+                }
+            }
+        }
+        return null;
+    }
 }
